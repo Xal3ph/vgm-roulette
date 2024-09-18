67591f037c97cc19cae65d17b9c3c7a746b6261c
Matthew Sabol <sabol.matthew@gmail.com>
2024-09-18 19:07:39 -0400

    fix: too much fun with this

--- src/app/vgm-roulette/vgm-roulette.component.ts
+++ src/app/vgm-roulette/vgm-roulette.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 // import genres from '../../../assets/data/genres.json'
 import genresCat from '../../assets/data/genres-categorized.json'
 
@@ -38,11 +38,15 @@ interface Game {
   templateUrl: './vgm-roulette.component.html',
   styleUrl: './vgm-roulette.component.scss'
 })
-export class VgmRouletteComponent {
+export class VgmRouletteComponent implements OnInit {
   genreCat: string = ""
   genre: string = ""
   game?: Game
-
+  @ViewChild('clouds1') clouds1!: ElementRef;
+  @ViewChild('clouds2') clouds2!: ElementRef;
+  @ViewChild('clouds3') clouds3!: ElementRef;
+  @ViewChild('cloudLonely') cloudLonely!: ElementRef;
+  startTime = new Date();
   token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxNzU1ZWQzZDFjOWE1MmM3NWZiOGYwYWZhYTZiYjcxMSIsImp0aSI6ImYyN2MyZjkxNjY0ZGY4ZWIwYjNjNzFlODgxMGJhOTg5NzRkMTY0ZDIyNDI0OGI4Njg1ZDI3YmI2MmJiMTIwYmNhMjY1ZDI4YTkwYjJmZjlhIiwiaWF0IjoxNzI2Njg0MDE4LjYyMzM5MSwibmJmIjoxNzI2Njg0MDE4LjYyMzM5NSwiZXhwIjozMzI4MzU5MjgxOC42MjA1ODMsInN1YiI6IjE1NDEyNjQiLCJpc3MiOiJodHRwczovL21ldGEud2lraW1lZGlhLm9yZyIsInJhdGVsaW1pdCI6eyJyZXF1ZXN0c19wZXJfdW5pdCI6NTAwMCwidW5pdCI6IkhPVVIifSwic2NvcGVzIjpbImJhc2ljIl19.jx3-nWrMZP3Q5N6dKnYExfIgo5uKC1-GNNQ16--nnKdpEWdpvTjGhJwOCf4uJKdYGueStRIKzilUdCEQGzPin9_MPvmMjJS5Z16C078P9ybnJ80XsRn-BljA6gWV_5Vr_UNMdptromH_hsW8v0p2RAdZlc20mL_p9lgsUGni0_Q9NgPKew35fD7xeAfzkR0-cLQf2BOSviRUaRnIg8QCfnZcliFGL6Alzo04X8-f_88-ZRpBXBysPywjSAx4sTYQWMsySW4ZwGIXWYOt2HfZ3VtuADtx6TD6GhPMGHD8kM8l7b5LOQPL5q4y2AiWWDpKce3b_6gR7eOBt52F6bKKM1gvl6xL8cJNnJ4-kpw6kjWd3amY5aG2XBOglwKx96TUhE-Glvu37ex3hjKnd5dPNNfaboPcQwyjnYrOo8eYP51QxJ75WqFDOArAx5bpdWtsms-nMV2LqfbhiNoWNaQlmbDpw9gJJvetlD9Kn41xVcNAj0dGDzWLwce_XgfG-V9m5Llm8fHM60SxzoiFJ5KhthDk-BhJV68VrIgQowr2C3EfT78UHT1wvTGpl3jnrWI3VihSuHYTZV6bqbsYokPPOKOQJHeKmOykFyEgbWp1JnHDgmw6IsPNzIbwD7cgaE-7T3WaDimByImOua4GpeGWNfROM02ey1OBiwuYjsLwAnU"
 
   
@@ -50,12 +54,9 @@ export class VgmRouletteComponent {
     return [...SNESGames, ...NESGames, ...SegaGenesisGames, ...PS1Games].filter(x => x.GameLink != undefined) as any[]
   }
 
-  constructor(library: FaIconLibrary, private http: HttpClient) {
+  constructor(library: FaIconLibrary, private http: HttpClient, private renderer: Renderer2) {
     library.addIcons(faWikipediaW);
     delete (genresCat as any)['Comedy'];
-  }
-
-  ngOnInit(): void {
   }
 
   // getRandomGenre(): string {
@@ -161,4 +162,24 @@ export class VgmRouletteComponent {
     this.rollGame()
   }
 
+  ngOnInit(): void {
+    this.setTimer();
+  }
+
+  private setTimer() {
+    setTimeout(() => {
+      // console.log(this.clouds1.nativeElement.style);
+      let dt = (new Date().getTime() - this.startTime.getTime())/1000;
+      let cloud1Width = (10*(dt))%this.clouds1.nativeElement.getBoundingClientRect().width;
+      let cloud2Width = (5*(dt))%this.clouds2.nativeElement.getBoundingClientRect().width;
+      let cloud3Width = ((dt))%this.clouds3.nativeElement.getBoundingClientRect().width;
+      let cloudLWidth = ((dt))%this.cloudLonely.nativeElement.getBoundingClientRect().width;
+      // this.renderer.setStyle(this.clouds1.nativeElement, 'transform', `translateX(${ time }px)`);
+      this.renderer.setStyle(this.clouds1.nativeElement, 'background-position-x', `${ cloud1Width }px`);
+      this.renderer.setStyle(this.clouds2.nativeElement, 'background-position-x', `${ cloud2Width }px`);
+      this.renderer.setStyle(this.clouds3.nativeElement, 'background-position-x', `${ cloud3Width }px`);
+      this.renderer.setStyle(this.cloudLonely.nativeElement, 'background-position-x', `${ cloudLWidth }px`);
+      this.setTimer();
+    }, 100);
+  }
 }
